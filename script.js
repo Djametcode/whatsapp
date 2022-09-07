d6f9c3160f68b0e740567b63994b79f7453e7d39
Djametcode <kampretsiji1@gmai.com>
2022-09-07 14:20:25 +0700

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// window.addEventListener('load', function(){
+   
+//     const user = prompt('Siapa Nama kamu?');
+//     const Asal = prompt('Asal mana?');
+//     alert('hallo' + '' + user +'' + 'dari' + Asal);
+// })
